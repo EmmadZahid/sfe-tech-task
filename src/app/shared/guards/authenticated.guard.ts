dbfe86ca976c4e61129db3d8e98be81fef7e0f39
emmadZahid <emmad.zahid@gmail.com>
2025-07-01 16:11:10 +0500

    Fixed the injectable for Guard

--- src/app/shared/guards/authenticated.guard.ts
+++ src/app/shared/guards/authenticated.guard.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "../../core/services/auth.service";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class AuthenticatedGuard implements CanActivate {
   authService: AuthService = inject(AuthService);
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
