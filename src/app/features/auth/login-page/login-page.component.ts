17e01a660934635b2ed312952933e63f2550a26a
emmadZahid <emmad.zahid@gmail.com>
2025-07-01 16:28:43 +0500

    Removing error on any input change or login click and using takeUntilDestroyed for preventing memory leaks

--- src/app/features/auth/login-page/login-page.component.ts
+++ src/app/features/auth/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, WritableSignal } from "@angular/core";
+import { Component, DestroyRef, inject, OnInit, signal, WritableSignal } from "@angular/core";
 import { MatFormField, MatInput, MatLabel } from "@angular/material/input";
 import { MatCard } from "@angular/material/card";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
@@ -11,6 +11,7 @@ import { forbiddenWordValidator } from "../../../shared/validators/forbidden-wor
 import { whitespaceValidator } from "../../../shared/validators/white-space-validator";
 import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 import { AuthResponse } from "../../../shared/models/auth";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-login-page",
@@ -27,29 +28,41 @@ import { AuthResponse } from "../../../shared/models/auth";
   templateUrl: "./login-page.component.html",
   styleUrl: "./login-page.component.scss",
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
   private fb: FormBuilder = inject(FormBuilder);
   private authService: AuthService = inject(AuthService);
   private router: Router = inject(Router);
   error: WritableSignal<string> = signal("");
   authenticating: WritableSignal<boolean> = signal(false);
-
+  private destroyRef = inject(DestroyRef);
   form = this.fb.group({
     username: ["", [Validators.required, whitespaceValidator, nospaceValidator, forbiddenWordValidator(["test"])]],
     password: ["", [Validators.required]],
   });
 
-  submit(): void {
-    this.authenticating.set(true);
-    this.authService.login(this.form.value.username ?? "", this.form.value.password ?? "").subscribe({
-      next: (res: AuthResponse) => {
-        this.authenticating.set(false);
-        this.router.navigate(["users"]);
-      },
-      error: (errRes: HttpErrorResponse) => {
-        this.error.set(errRes.error?.message || "Some error occurred");
-        this.authenticating.set(false);
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+      next: value => {
+        this.error.set("");
       },
     });
+  }
+
+  submit(): void {
+    this.error.set("");
+    this.authenticating.set(true);
+    this.authService
+      .login(this.form.value.username ?? "", this.form.value.password ?? "")
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (res: AuthResponse) => {
+          this.authenticating.set(false);
+          this.router.navigate(["users"]);
+        },
+        error: (errRes: HttpErrorResponse) => {
+          this.error.set(errRes.error?.message || "Some error occurred");
+          this.authenticating.set(false);
+        },
+      });
   }
 }
