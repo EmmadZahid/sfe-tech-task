f7b6b28ee199f1239984ae1acaa26a62ab79a638
emmadZahid <emmad.zahid@gmail.com>
2025-07-01 23:18:40 +0500

    Added delay for all API to handle loading states

--- src/app/shared/interceptors/auth-interceptor.ts
+++ src/app/shared/interceptors/auth-interceptor.ts
@@ -1,12 +1,16 @@
 import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
 import { AuthService } from "../../core/services/auth.service";
 import { inject } from "@angular/core";
+import { delay } from "rxjs";
 
+const DELAY: number = 1000;
 export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
   const authToken = inject(AuthService).token();
-
+  if (req.url.includes("/login")) {
+    return next(req).pipe(delay(DELAY));
+  }
   const newReq = req.clone({
     headers: req.headers.append("authorization", `BEARER ${authToken}`),
   });
-  return next(newReq);
+  return next(newReq).pipe(delay(DELAY));
 }
