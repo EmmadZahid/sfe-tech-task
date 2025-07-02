9cf86ecc9361a661cfed2c7079f3373638118271
emmadZahid <emmad.zahid@gmail.com>
2025-07-02 13:53:34 +0500

    Fixed the null injection

--- src/app/shared/guards/edit.guard.ts
+++ src/app/shared/guards/edit.guard.ts
@@ -2,9 +2,8 @@ import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, Router } from "@angular/router";
 import { UsersFacadeService } from "../../core/facades/users-facade.service";
 
-export const editGuard: CanActivateFn = () => {
+export const editGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const facade = inject(UsersFacadeService);
-  const route = inject(ActivatedRouteSnapshot);
   const router = inject(Router);
   const hasPermission =
     facade.isLoggedInUserAdmin() || route.paramMap.get("id") == facade.loggedInUser()?.id.toString();
