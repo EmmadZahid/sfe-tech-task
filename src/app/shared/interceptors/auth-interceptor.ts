a3e2dfc06509cc13bbc2dafe1fb54b1fd89367b4
emmadZahid <emmad.zahid@gmail.com>
2025-07-02 05:00:58 +0500

    Added 401 handling in interceptor

--- src/app/shared/interceptors/auth-interceptor.ts
+++ src/app/shared/interceptors/auth-interceptor.ts
@@ -1,16 +1,28 @@
-import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpHandlerFn, HttpRequest } from "@angular/common/http";
 import { inject } from "@angular/core";
-import { delay } from "rxjs";
+import { catchError, delay, throwError } from "rxjs";
 import { AuthFacadeService } from "../../core/facades/auth-facade.service";
+import { Router } from "@angular/router";
 
 const DELAY: number = 500;
 export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
-  const token = inject(AuthFacadeService).token();
+  const authFacade = inject(AuthFacadeService);
+  const router = inject(Router);
   if (req.url.includes("/login")) {
     return next(req).pipe(delay(DELAY));
   }
   const newReq = req.clone({
-    headers: req.headers.append("authorization", `BEARER ${token}`),
+    headers: req.headers.append("authorization", `BEARER ${authFacade.token()}`),
   });
-  return next(newReq).pipe(delay(DELAY));
+  return next(newReq).pipe(
+    delay(DELAY),
+    catchError((err: HttpErrorResponse) => {
+      if (err.status === 401) {
+        authFacade.logout();
+        router.navigateByUrl("/auth");
+      }
+
+      return throwError(() => err);
+    })
+  );
 }
